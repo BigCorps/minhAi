d92c162edbf5b27e92113b302e98b5879d18ed58
BigCorps <interpag.bigcorps@gmail.com>
2026-01-15 18:30:35 +0000

    av11

--- components/AvatarFace.tsx
+++ components/AvatarFace.tsx
@@ -72,7 +72,7 @@ export function AvatarFace({ isListening, isSpeaking, isProcessing }: AvatarFace
           {/* GRUPO DE ESCALA 1.5x MANTIDO */}
           <g style={{ transform: 'scale(1.5)', transformOrigin: '32px 32px' }}>
             
-            {/* OLHO ESQUERDO: Ajustado para o tamanho médio 7x11 */}
+            {/* OLHO ESQUERDO (7x11) - Brilho grande reduzido */}
             <g 
               transform={`translate(22, ${20 + eyeOffsetY})`} 
               className="transition-transform duration-500 ease-in-out"
@@ -81,16 +81,16 @@ export function AvatarFace({ isListening, isSpeaking, isProcessing }: AvatarFace
                 <rect x="0" y="5" width="7" height="2" fill={featureColor} />
               ) : (
                 <>
-                  {/* Path do olho 7x11 */}
                   <path d="M1,0 H6 V1 H7 V10 H6 V11 H1 V10 H0 V1 H1 Z" fill={featureColor} />
-                  {/* Brilhos proporcionais ao 7x11 */}
-                  <rect x="1" y="1" width="3" height="4" fill="white" />
+                  {/* Brilho grande reduzido para 2x3 */}
+                  <rect x="1" y="1" width="2" height="3" fill="white" />
+                  {/* Brilho pequeno mantido */}
                   <rect x="5" y="8" width="1" height="1" fill="white" />
                 </>
               )}
             </g>
 
-            {/* OLHO DIREITO: Ajustado para o tamanho médio 7x11 */}
+            {/* OLHO DIREITO (7x11) - Brilho grande reduzido */}
             <g 
               transform={`translate(35, ${20 + eyeOffsetY})`}
               className="transition-transform duration-500 ease-in-out"
@@ -99,10 +99,10 @@ export function AvatarFace({ isListening, isSpeaking, isProcessing }: AvatarFace
                  <rect x="0" y="5" width="7" height="2" fill={featureColor} />
               ) : (
                 <>
-                  {/* Path do olho 7x11 */}
                   <path d="M1,0 H6 V1 H7 V10 H6 V11 H1 V10 H0 V1 H1 Z" fill={featureColor} />
-                  {/* Brilhos proporcionais ao 7x11 */}
-                  <rect x="1" y="1" width="3" height="4" fill="white" />
+                  {/* Brilho grande reduzido para 2x3 */}
+                  <rect x="1" y="1" width="2" height="3" fill="white" />
+                  {/* Brilho pequeno mantido */}
                   <rect x="5" y="8" width="1" height="1" fill="white" />
                 </>
               )}

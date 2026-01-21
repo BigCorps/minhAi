9baa08d572b48c95c0725fd8aa93270531324a3b
BigCorps <interpag.bigcorps@gmail.com>
2026-01-21 23:28:50 +0000

    fix1

--- components/CreditsCard.tsx
+++ components/CreditsCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { createClient } from '@/lib/supabase-client';
+import { createClient } from '@/lib/supabase-browser';
 import { Zap, TrendingUp } from 'lucide-react';
 import Link from 'next/link';
 

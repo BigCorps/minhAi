7f88ac1adbd3dcd62ac1d5ffb2068d0a94649874
BigCorps <interpag.bigcorps@gmail.com>
2026-01-23 12:03:52 -0300

    Update UserProfile.tsx

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -17,7 +17,7 @@ export function UserProfile({ user }: UserProfileProps) {
 
   async function handleLogout() {
     await supabase.auth.signOut();
-    window.location.href = 'https://itend.com.br';
+    window.location.href = 'https://eai.app.br';
   }
 
   const displayName = user.user_metadata?.name || user.user_metadata?.full_name || user.email?.split('@')[0] || 'Usuário';

beda72b608a3d44f38e48c60ec1d1f8cb6746dbd
BigCorps <interpag.bigcorps@gmail.com>
2026-01-23 12:05:48 -0300

    Update page.tsx

--- app/dashboard/faqs/[id]/page.tsx
+++ app/dashboard/faqs/[id]/page.tsx
@@ -34,7 +34,7 @@ export default async function CompanyFAQsPage({ params }: { params: { id: string
               <Link href="/dashboard">
                 <Image 
                   src="/logo.png" 
-                  alt="iTend" 
+                  alt="eAi" 
                   width={150} 
                   height={68}
                   className="rounded-lg cursor-pointer"

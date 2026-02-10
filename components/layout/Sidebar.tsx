7a318e06ed70cfd5fe9a12e290be508ba7af5e9a
BigCorps <interpag.bigcorps@gmail.com>
2026-02-10 12:57:09 +0000

    go3

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -66,7 +66,7 @@ export function Sidebar() {
               <h3 className={`text-sm font-semibold ${
                 theme === 'dark' ? 'text-white' : 'text-gray-900'
               }`}>
-                Navegação
+                eAi.app.br
               </h3>
             </div>
 

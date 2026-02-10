43ae4169ed42db657e2b4ec65df1bfd3eeb4afa5
BigCorps <interpag.bigcorps@gmail.com>
2026-02-10 17:47:05 -0300

    Atualizar o DashboardHeader.tsx

--- components/layout/DashboardHeader.tsx
+++ components/layout/DashboardHeader.tsx
@@ -86,7 +86,7 @@ export function DashboardHeader({ user }: DashboardHeaderProps) {
                         <span>Perfil</span>
                       </Link>
                       <Link
-                        href="/credits"
+                        href="/dashboard/credits"
                         className="flex items-center space-x-3 px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-white/5"
                         onClick={() => setUserMenuOpen(false)}
                       >

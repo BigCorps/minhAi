d7eccce0b8b75dd2b5bf1406e3b375acc02dfbfb
BigCorps <interpag.bigcorps@gmail.com>
2026-02-12 16:28:06 +0000

    ia3

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl border bg-card text-card-foreground shadow",
+      "rounded-xl border bg-white text-gray-900 shadow dark:bg-gray-800 dark:text-gray-100 dark:border-gray-700",
       className
     )}
     {...props}

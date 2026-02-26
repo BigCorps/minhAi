291552cf3664bac80dbe836f06135a9e0cb913e7
BigCorps <interpag.bigcorps@gmail.com>
2026-02-26 11:09:04 -0300

    Update index.ts

--- supabase/functions/confirmar-pagamento-infinitepay/index.ts
+++ supabase/functions/confirmar-pagamento-infinitepay/index.ts
@@ -1,5 +1,4 @@
 // supabase/functions/confirmar-pagamento-infinitepay/index.ts
-import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 const corsHeaders = {

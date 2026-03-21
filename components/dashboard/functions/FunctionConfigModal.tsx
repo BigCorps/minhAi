3b54ae24371325e7fca0d6f669d57fdfab5685c5
BigCorps <interpag.bigcorps@gmail.com>
2026-03-21 19:32:47 -0300

    Update FunctionConfigModal.tsx

--- components/dashboard/functions/FunctionConfigModal.tsx
+++ components/dashboard/functions/FunctionConfigModal.tsx
@@ -2764,8 +2764,7 @@ export default function FunctionConfigModal({
       setIsLoading(true);
       const { data, error } = await supabase
         .from('companies')
-        .select('whatsapp_number, instagram_username, website, facebook, email_contato, linkedin, tiktok, twitter, telefone_fixo, receiving_pix_key, receiving_pix_key_type, system_prompt, orcamento_prompt, brand_description, business_hours, business_address, video_instrucoes_url, sequencia_videos_urls, infinitepay_handle, wifi_network_name, wifi_network_password, cardapio_url, cardapio_description, validar_cupom, qrcode_content, qrcode_label, print_charge_enabled,print_price_per_page,print_max_pages_per_job, printnode_api_key,printnode_printer_id,thermal_printer_id, thermal_connection_type')
-        .eq('id', companyId)
+.select('whatsapp_number, instagram_username, website, facebook, email_contato, linkedin, tiktok, twitter, telefone_fixo, receiving_pix_key, receiving_pix_key_type, system_prompt, orcamento_prompt, brand_description, business_hours, business_address, video_instrucoes_url, sequencia_videos_urls, infinitepay_handle, wifi_network_name, wifi_network_password, cardapio_url, cardapio_description, validar_cupom, qrcode_content, qrcode_label, manual_payment_enabled, print_price_per_page, print_max_pages_per_job, print_color_enabled, print_price_bw, print_price_color, printnode_computer_id, printnode_printer_id_bw, printnode_printer_id_color, thermal_printer_id, thermal_connection_type')        .eq('id', companyId)
         .single();
 
       if (data) {

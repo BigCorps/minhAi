51bebcc2af9a479955fd258ed9f94eb82a80071b
BigCorps <interpag.bigcorps@gmail.com>
2026-04-05 20:41:41 -0300

    Atualizar o GerenteDashboard.tsx

--- components/cliente/dashboards/GerenteDashboard.tsx
+++ components/cliente/dashboards/GerenteDashboard.tsx
@@ -46,7 +46,7 @@ export default function GerenteDashboard({ profile, company, theme }: Props) {
           </p>
         </div>
         <div className="flex-shrink-0 ml-4">
-          <BotaoLogout slug={company.slug} theme={theme} compact />
+          <BotaoLogout slug={company.slug} theme={theme} profile={profile} compact />
         </div>
       </div>
 

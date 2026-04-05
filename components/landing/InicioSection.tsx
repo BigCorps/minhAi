136ec463aaa917ae711de81dabee9f1ad423ce88
BigCorps <interpag.bigcorps@gmail.com>
2026-04-05 20:15:57 -0300

    Atualizar o InicioSection.tsx

--- components/landing/InicioSection.tsx
+++ components/landing/InicioSection.tsx
@@ -83,7 +83,7 @@ export default function InicioSection({ theme = 'dark' }: InicioSectionProps) {
   <span
     className={`
       ${isDark ? 'text-blue-400' : 'text-blue-600'}
-      text-6xl md:text-3xl lg:text-8xl font-bold
+      text-5xl md:text-3xl lg:text-5xl font-bold
     `}
   >
     Uma IA pra chamar de sua!

10cba6e2cfd3e9c5bc4fdfeb9b8c842e2710cc42
BigCorps <interpag.bigcorps@gmail.com>
2026-04-05 20:41:11 -0300

    Atualizar o CaixaDashboard.tsx

--- components/cliente/dashboards/CaixaDashboard.tsx
+++ components/cliente/dashboards/CaixaDashboard.tsx
@@ -44,7 +44,7 @@ export default function CaixaDashboard({ profile, company, theme }: Props) {
           </p>
         </div>
         <div className="flex-shrink-0 ml-4">
-          <BotaoLogout slug={company.slug} theme={theme} compact />
+          <BotaoLogout slug={company.slug} theme={theme} profile={profile} compact />
         </div>
       </div>
 

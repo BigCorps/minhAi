2efc9075f8c0983db9dafb9f153787079b4809e5
BigCorps <interpag.bigcorps@gmail.com>
2026-04-10 12:00:24 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,2 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', () => self.clients.claim());
-// Sem handler de fetch — deixa o OneSignalSDK.sw.js controlar tudo

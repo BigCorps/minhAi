98533bdd302242c898f2f57ed34d74d7bb7421ca
BigCorps <interpag.bigcorps@gmail.com>
2026-04-15 13:16:29 -0300

    Update VoiceAssistantWithWakeWord.tsx

--- components/VoiceAssistant/VoiceAssistantWithWakeWord.tsx
+++ components/VoiceAssistant/VoiceAssistantWithWakeWord.tsx
@@ -635,7 +635,7 @@ case 'fila_atendimento':
   break;
 
 case 'gerar_senha':
-  await stopGoogleSpeech();
+  stopGoogleSpeech();
   setActiveModal({
     type: 'GerarSenhaDisplay',
     data: { companyId, slug },

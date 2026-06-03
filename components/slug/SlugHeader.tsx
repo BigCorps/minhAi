6912c375c8399f2b8a10bd14df0a186caddf2c5c
BigCorps <interpag.bigcorps@gmail.com>
2026-06-03 18:33:26 -0300

    Atualizar o SlugHeader.tsx

--- components/slug/SlugHeader.tsx
+++ components/slug/SlugHeader.tsx
@@ -281,7 +281,7 @@ export default function SlugHeader({
           <button onClick={handleNavigateToIA} className={btn()} title="Ir para Assistente">
             <svg className={sz} fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456z" />
             </svg>
           </button>
         )}

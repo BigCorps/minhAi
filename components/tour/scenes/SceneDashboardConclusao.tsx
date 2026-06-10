0c926b17fb2b0405c3d12a5c20288ebb3cd7a415
BigCorps <interpag.bigcorps@gmail.com>
2026-06-10 18:14:53 +0000

    5T8

--- components/tour/scenes/SceneDashboardConclusao.tsx
+++ components/tour/scenes/SceneDashboardConclusao.tsx
@@ -609,8 +609,9 @@ export default function SceneDashboardVisao() {
             ))}
           </div>
 
-      </div>
-    </div>  {/* ← fecha o novo div interno (visual) */}
-  </div>    {/* ← fecha o wrapper externo (scale) */}
+        </div> {/* ← fecha o wrapper do BODY */}
+      </div>   {/* ← fecha o div interno de coluna (visual) */}
+    </div>     {/* ← fecha o wrapper externo (scale) */}
+  </div>       {/* ← fecha o containerRef (raiz) */}
   )
 }

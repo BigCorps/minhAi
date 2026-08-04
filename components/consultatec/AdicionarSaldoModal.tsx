4d4684a1f5911cca0dd05189e691f4f3d1132f03
BigCorps <interpag.bigcorps@gmail.com>
2026-08-04 14:06:09 -0300

    Update AdicionarSaldoModal.tsx

--- components/consultatec/AdicionarSaldoModal.tsx
+++ components/consultatec/AdicionarSaldoModal.tsx
@@ -32,8 +32,8 @@ const cor = {
   borda: '#C9BFA0',
   tinta: '#1C1A14',
   tintaMuted: '#6B6350',
-  destaque: '#2F4F3A',
-  destaqueHover: '#25402E',
+  destaque: '#7A6142',
+  destaqueHover: '#7A6142',
   erroBg: '#F4E4E0',
   erroTexto: '#7A2E2E',
 };

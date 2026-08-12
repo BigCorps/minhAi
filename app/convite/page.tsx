e9e9231853583b4cbe8ef87522dc6061422f6454
BigCorps <interpag.bigcorps@gmail.com>
2026-08-12 20:06:02 -0300

    Update page.tsx

--- app/convite/page.tsx
+++ app/convite/page.tsx
@@ -29,7 +29,7 @@ export default function PaginaInicialConvite() {
       {/* Logo Centralizado */}
       <div className="mb-8 flex justify-center">
         <Image
-          src="/brands/convite/marca-256.png"
+          src="/brands/convite/icone-256.png"
           alt="Logo Convite IA"
           width={128}
           height={128}
